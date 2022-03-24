f0533050323862da8a5f14cae028357a0e78f916
bomendez <mendez.bo@husky.neu.edu>
2022-03-23 22:28:19 -0700

    JedisPool created and schema temp set

--- Consumer/src/main/java/ReceiverThread.java
+++ Consumer/src/main/java/ReceiverThread.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import java.io.IOException;
 import java.util.Map;
@@ -16,35 +18,43 @@ public class ReceiverThread implements Runnable {
     private String queue_name;
     private Map<Integer, Integer> threadMap;
     private Gson gson = new Gson();
-    private Jedis jedis;
+    private JedisPool pool;
+    private Integer VERTICAL_CONVERSION = 10;
 
 
-    public ReceiverThread(ConnectionFactory fact, final Connection conn, String qname, Map<Integer, Integer> map, Jedis mjedis) {
+    public ReceiverThread(ConnectionFactory fact, final Connection conn, String qname, Map<Integer, Integer> map, JedisPool jpool) {
         factory = fact;
         connection = conn;
         queue_name = qname;
         threadMap = map;
-        jedis = mjedis;
+        pool = jpool;
     }
 
     public void run() {
+        Jedis redis = null;
         try {
             final Channel channel = connection.createChannel();
             channel.queueDeclare(queue_name, false, false, false, null);
             // max one message per receiver
             channel.basicQos(1);
             System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
+            redis = pool.getResource();
 
+            Jedis finalRedis = redis;
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                 String message = new String(delivery.getBody(), "UTF-8");
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
                 Skiers skiers = (Skiers) gson.fromJson(message, Skiers.class);
                 Integer skierID = skiers.getSkierID();
-                Integer liftID = skiers.getLiftID();
-                System.out.println("skierID " + skierID + "liftID " + liftID);
+                String dayID = skiers.getDayID();
+                String seasonID = skiers.getSeasonID();
+                Integer liftID = skiers.getLiftID() * VERTICAL_CONVERSION;
 //                threadMap.put(skierID, liftID);
-                jedis.set(skierID.toString(), liftID.toString());
-                String value = jedis.get(skierID.toString());
+                System.out.println("line 51");
+                finalRedis.hset(skierID.toString(), dayID, liftID.toString());
+                finalRedis.hset(skierID.toString(), seasonID, dayID);
+                String dayIDRedis = finalRedis.hget(skierID.toString(), dayID);
+                System.out.println(" dayID:" + dayIDRedis);
             };
             // process messages
             channel.basicConsume(queue_name, false, deliverCallback, consumerTag -> {
@@ -52,6 +62,19 @@ public class ReceiverThread implements Runnable {
 
         } catch (IOException ex) {
             Logger.getLogger(RecvMT.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JedisConnectionException e) {
+            if (redis != null)
+            {
+                pool.returnBrokenResource(redis);
+                redis = null;
+            }
+            throw e;
+        } finally
+        {
+            if (redis != null)
+            {
+                pool.returnResource(redis);
+            }
         }
     }
 }
--- Consumer/src/main/java/RecvMT.java
+++ Consumer/src/main/java/RecvMT.java
@@ -4,6 +4,9 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.util.Map;
 import java.util.concurrent.*;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
 public class RecvMT {
 
@@ -11,9 +14,12 @@ public class RecvMT {
     private final static int NUM_THREADS = 8;
     private static Map<Integer, Integer> map = new ConcurrentHashMap(20000);
 
+
     public static void main(String[] argv) throws Exception {
-        Jedis jedis = new Jedis("54.213.252.120", 6379);
-        jedis.auth("p@ss$12E45");
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "54.213.252.120", 6379, Protocol.DEFAULT_TIMEOUT, "p@ss$12E45");
+
+//        Jedis jedis = new Jedis("54.213.252.120", 6379);
+//        jedis.auth("p@ss$12E45");
         System.out.println("Connected to Redis");
 
         ConnectionFactory factory = new ConnectionFactory();
@@ -30,7 +36,7 @@ public class RecvMT {
         ExecutorService consumerPool = Executors.newFixedThreadPool(NUM_THREADS);
 
         for (int i = 0; i< NUM_THREADS; i++) {
-            ReceiverThread receiverThread = new ReceiverThread(factory, connection, QUEUE_NAME, map, jedis);
+            ReceiverThread receiverThread = new ReceiverThread(factory, connection, QUEUE_NAME, map, pool);
             consumerPool.execute(receiverThread);
         }
     }
