460984f64c59f98848d80ab9f4ea87af9366e9a3
bomendez <mendez.bo@husky.neu.edu>
2022-03-31 16:01:52 -0700

    queue exchange commented

--- Consumer/src/main/java/ReceiverThread.java
+++ Consumer/src/main/java/ReceiverThread.java
@@ -16,17 +16,18 @@ import java.util.logging.Logger;
 public class ReceiverThread implements Runnable {
     private ConnectionFactory factory;
     private Connection connection;
-    private String queue_name;
+    private String EXCHANGE_NAME;
+    private String QUEUE_NAME = "skier";
     private Map<Integer, Integer> threadMap;
     private Gson gson = new Gson();
     private JedisPool pool;
     private Integer VERTICAL_CONVERSION = 10;
 
 
-    public ReceiverThread(ConnectionFactory fact, final Connection conn, String qname, Map<Integer, Integer> map, JedisPool jpool) {
+    public ReceiverThread(ConnectionFactory fact, final Connection conn, String eExch, Map<Integer, Integer> map, JedisPool jpool) {
         factory = fact;
         connection = conn;
-        queue_name = qname;
+        EXCHANGE_NAME = eExch;
         threadMap = map;
         pool = jpool;
     }
@@ -35,17 +36,26 @@ public class ReceiverThread implements Runnable {
         Jedis redis = null;
         try {
             final Channel channel = connection.createChannel();
-            channel.queueDeclare(queue_name, false, false, false, null);
-            // max one message per receiver
-            channel.basicQos(1);
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//            channel.queueDeclare(EXCHANGE_NAME, false, false, false, null);
+//            channel.basicQos(1);
+
+
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            String queueName = "skier";
+//            String queueName = channel.queueDeclare().getQueue();
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
             System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
             redis = pool.getResource();
 
             Jedis finalRedis = redis;
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                 String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println("message" + message);
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                System.out.println("after ack");
                 Skiers skiers = (Skiers) gson.fromJson(message, Skiers.class);
+                System.out.println("gson parsed " +skiers.getSkierID().toString());
                 Integer skierID = skiers.getSkierID();
                 String dayID = skiers.getDayID();
                 String seasonID = skiers.getSeasonID();
@@ -53,8 +63,10 @@ public class ReceiverThread implements Runnable {
 //                threadMap.put(skierID, liftID);
                 String skierSeasonID = skierID.toString() + ":" + seasonID.toString();
                 finalRedis.lpush(skierSeasonID, dayID);
+                System.out.println("pushed to redis");
                 List<String> dayList = finalRedis.lrange(skierSeasonID, 0, -1);
                 System.out.println("dayList " + dayList);
+                System.out.println("dayList length: " + dayList.size());
                 finalRedis.lpush(skierID.toString(), liftID.toString());
                 List<String> liftIDList = finalRedis.lrange(skierID.toString(), 0, -1);
                 Integer verticalTotal = liftID * 10;
@@ -65,12 +77,16 @@ public class ReceiverThread implements Runnable {
 
             };
             // process messages
-            channel.basicConsume(queue_name, false, deliverCallback, consumerTag -> {
+//            channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {
+//            });
+            channel.basicConsume(EXCHANGE_NAME, false, deliverCallback, consumerTag -> {
             });
 
         } catch (IOException ex) {
+            System.out.println("Logger Exception");
             Logger.getLogger(RecvMT.class.getName()).log(Level.SEVERE, null, ex);
         } catch (JedisConnectionException e) {
+            System.out.println("JedisConnectionException");
             if (redis != null)
             {
                 pool.returnBrokenResource(redis);
--- Consumer/src/main/java/RecvMT.java
+++ Consumer/src/main/java/RecvMT.java
@@ -3,32 +3,29 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.util.Map;
 import java.util.concurrent.*;
-import redis.clients.jedis.Jedis;
+
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
 public class RecvMT {
-
+    private static String EXCHANGE_NAME = "skier";
     private final static String QUEUE_NAME = "ski-rabbit";
     private final static int NUM_THREADS = 8;
+    private final static String REDIS_IP = "35.160.13.77";
+
+    private final static String rabbitMQIP = "localhost";
     private static Map<Integer, Integer> map = new ConcurrentHashMap(20000);
 
 
     public static void main(String[] argv) throws Exception {
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), "54.213.252.120", 6379, Protocol.DEFAULT_TIMEOUT, "p@ss$12E45");
-
-//        Jedis jedis = new Jedis("54.213.252.120", 6379);
-//        jedis.auth("p@ss$12E45");
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), REDIS_IP, 6379, Protocol.DEFAULT_TIMEOUT);
         System.out.println("Connected to Redis");
 
         ConnectionFactory factory = new ConnectionFactory();
-//        factory.setHost("localhost");
-//        factory.setUsername("guest");
-//        factory.setPassword("guest");
-        factory.setHost("54.68.165.222");
-        factory.setUsername("radmin");
-        factory.setPassword("radmin");
+        factory.setHost(rabbitMQIP);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
 
         final Connection connection = factory.newConnection();
 
@@ -36,7 +33,8 @@ public class RecvMT {
         ExecutorService consumerPool = Executors.newFixedThreadPool(NUM_THREADS);
 
         for (int i = 0; i< NUM_THREADS; i++) {
-            ReceiverThread receiverThread = new ReceiverThread(factory, connection, QUEUE_NAME, map, pool);
+            ReceiverThread receiverThread = new ReceiverThread(factory, connection, EXCHANGE_NAME, map, pool);
+//            ReceiverThread receiverThread = new ReceiverThread(factory, connection, QUEUE_NAME, map, pool);
             consumerPool.execute(receiverThread);
         }
     }
